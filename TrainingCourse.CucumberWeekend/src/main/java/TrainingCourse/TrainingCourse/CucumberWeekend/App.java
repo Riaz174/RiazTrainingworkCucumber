bf50637918c6455342f41cad2a540e3bd949f460
Dell <Dell@DESKTOP-2U6K5SH.myfiosgateway.com>
2022-04-05 12:52:56 -0400

    New Project

---
+++ TrainingCourse.CucumberWeekend/src/main/java/TrainingCourse/TrainingCourse/CucumberWeekend/App.java
@@ -0,0 +1,13 @@
+package TrainingCourse.TrainingCourse.CucumberWeekend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
